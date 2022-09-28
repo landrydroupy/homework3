3a64bbb9fdff474ed2520905c0b6f674eff5a7c1
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-09-28 13:40:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
 <!doctype html>
+<?php include("nav.php");?>
+
+<h1> Welcome to my database website, this will contain 4 seperate tables describing different aspects of the university of oklahoma.<h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -6,52 +33,5 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th>courseid</th>
-      <th>prefix</th>
-      <th>number</th>
-      <th>description</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-$servername = "localhost";
-$username = "landryou_user";
-$password = "A2kYbmhiMHTE";
-$dbname = "landryou_homework3data";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT courseid,prefix,coursenumber,description FROM course";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-?>
-  <tr>
-    <td><?=$row["courseid"]?></td>
-    <td><?=$row["prefix"]?></td>
-     <td><?=$row["number"]?></td>
-    <td><?=$row["description"]?></td>
-  </tr>
-<?php
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
-?>
-  </tbody>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
-</html>
-
+ 
